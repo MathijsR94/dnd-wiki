5ba1920a46ce3e215ec96525b0dbb12a64f1aa07
Mathijs Rutgers <mathijsrutgers@gmail.com>
2019-09-23 19:00:51 +0200

    feat(editor): basic content editor

--- apps/frontend/src/components/contentEditor/components/Toolbar.tsx
+++ apps/frontend/src/components/contentEditor/components/Toolbar.tsx
@@ -1,58 +1,51 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {
-  ItalicButton,
-  BoldButton,
-  UnderlineButton,
-  OrderedListButton,
-  UnorderedListButton,
-  HeadlineOneButton,
-  HeadlineTwoButton,
-  HeadlineThreeButton,
+    ItalicButton,
+    BoldButton,
+    UnderlineButton,
+    OrderedListButton,
+    UnorderedListButton,
+    HeadlineOneButton,
+    HeadlineTwoButton,
+    HeadlineThreeButton,
+    CodeButton,
+    BlockquoteButton,
+    CodeBlockButton,
 } from 'draft-js-buttons';
-import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
-import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
-
+import createToolbarPlugin, {Separator} from 'draft-js-static-toolbar-plugin';
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
 import '../css/toolbar.css';
 
 type Props = {
-  onAddImage: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    onAddImage: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
-export const inlineToolbarPlugin = createInlineToolbarPlugin();
+
 export const staticToolbarPlugin = createToolbarPlugin();
-
-const { InlineToolbar } = inlineToolbarPlugin;
-
-const buttons = [
-  ItalicButton,
-  BoldButton,
-  UnderlineButton,
-  OrderedListButton,
-  UnorderedListButton,
-  HeadlineOneButton,
-  HeadlineTwoButton,
-  HeadlineThreeButton
-];
+const {Toolbar} = staticToolbarPlugin;
 
 const theme = {
-  active: 'toolbar__button toolbar__button--active',
-  button: 'toolbar__button',
-  buttonWrapper: 'toolbar__button-wrapper'
+    active: 'toolbar__button toolbar__button--active',
+    button: 'toolbar__button',
+    buttonWrapper: 'toolbar__button-wrapper',
 };
 
-export default ({ onAddImage }: Props) => (
-  <InlineToolbar>
-    {({ setEditorState, getEditorState }) => (
-      <React.Fragment>
-        {buttons.map(Button => (
-          <Button
-            key={Button.displayName}
-            theme={theme}
-            setEditorState={setEditorState}
-            getEditorState={getEditorState}
-          />
-        ))}
-        {/* <button className={theme.button} onClick={onAddImage}></button> */}
-      </React.Fragment>
-    )}
-  </InlineToolbar>
+export default ({onAddImage}: Props) => (
+    <Toolbar>
+        {(externalProps: any) => (
+            <Fragment>
+                <BoldButton theme={theme} {...externalProps} />
+                <ItalicButton theme={theme} {...externalProps} />
+                <UnderlineButton theme={theme} {...externalProps} />
+                <CodeButton theme={theme} {...externalProps} />
+                <Separator theme={theme} {...externalProps} />
+                <HeadlineOneButton theme={theme} {...externalProps} />
+                <HeadlineTwoButton theme={theme} {...externalProps} />
+                <HeadlineThreeButton theme={theme} {...externalProps} />
+                <UnorderedListButton theme={theme} {...externalProps} />
+                <OrderedListButton theme={theme} {...externalProps} />
+                <BlockquoteButton theme={theme} {...externalProps} />
+                <CodeBlockButton theme={theme} {...externalProps} />
+            </Fragment>
+        )}
+    </Toolbar>
 );
